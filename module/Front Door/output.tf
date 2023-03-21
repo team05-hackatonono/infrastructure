f0fe408e832d4182e623064c551ba830289d2d01
AntonyF-Andreani <afagundez@andreani.com>
2023-03-21 16:08:15 -0300

    fix: names

--- module/Front Door/output.tf
+++
@@ -1,3 +0,0 @@
-output "frontend_endpoints" {
-  value = azurerm_frontdoor.mshack.frontend_endpoint[0].name
-}
--- solution/vars.tf
+++ solution/vars.tf
@@ -1,12 +1,11 @@
 variable "availability_zone_names" {
-  type    = map
-  default = [{
-    us_east_2: "us-east-2", 
-  }, 
-  {
-    us_central: "us-central", 
-  },
-  {
-    bra_south: "bra-south"
-  }]
+ type = map
+ default = [{ us_east_2: "us-east-2",
+ },
+ {
+ us_central: "us-central",
+ },
+{
+ bra_south: "bra-south"
+}]
 }
