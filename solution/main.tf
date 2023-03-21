0ab97969009635eae668ed646f479f4284b75c26
gmolinas <90208075+gmolinas@users.noreply.github.com>
2023-03-21 14:38:56 -0300

    Add more

--- solution/main.tf
+++ solution/main.tf
@@ -2,3 +2,13 @@ resource "azurerm_resource_group" "az_rg_team5" {
 name = "az_rg_team5"
 location = "East US 2"
 }
+
+resource "azurerm_resource_group" "az_rg_team5_centralus" {
+name = "az_rg_team5_centralus"
+location = "South Brazil"
+}
+
+resource "azurerm_resource_group" "az_rg_team5_br" {
+name = "az_rg_team5_br"
+location = "South Brazil"
+}
